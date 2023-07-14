86f8bc17e24abda555124c38e6dac4aca85c61a7
frisyk <frisnadi1@gmail.com>
2023-07-15 06:33:57 +0700

    add features 2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,9 @@ import { BsInstagram, BsWhatsapp, BsGithub, BsDribbble } from 'react-icons/bs'
 
 export default function Footer() {
     return (
-        <footer className="flex items-center justify-between p-10 gap-10 text-xl font-bold text-gray-600">
+        <footer className="flex w-full items-center justify-between p-10 gap-3 lg:gap-10 text-sm lg:text-xl font-bold text-gray-600">
             <p>Frisyk.</p>
-            <section className="flex gap-8">
+            <section className="flex  gap-5 lg:gap-8">
                 <BsInstagram size={30} />
                 <BsWhatsapp size={30} />
                 <BsGithub size={30} />
