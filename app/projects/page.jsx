6912417ac980dc6bb45da4665f3a8c1e9827fda7
frisyk <frisnadi1@gmail.com>
2025-01-02 08:01:33 +0700

    refactor: add  projects

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,6 +1,7 @@
 import { projectData } from '@/components/Data';
 import Image from 'next/image';
 import Link from 'next/link';
+import { BsGithub } from 'react-icons/bs';
 
 export const metadata = {
   title: "Projects"
@@ -12,7 +13,7 @@ export default function ProjectsPage() {
       <section className='flex flex-wrap justify-center p-5'>
         {
           projectData.map((it) => (
-            <Project key={it.id} title={it.title} img={it.image} url={it.slug} desc={it.desc} />
+            <Project key={it.id} title={it.title} img={it.image} url={it.slug} desc={it.desc} github={it.github} />
           ))
         }
       </section>
@@ -20,7 +21,7 @@ export default function ProjectsPage() {
   );
 }
 
-function Project({ img, title, url, desc }) {
+function Project({ img, title, url, github, desc }) {
   return (
     <div className="bg-white shadow-md m-5 w-full md:w-1/2 lg:w-1/3 xl:w-1/3 rounded-lg overflow-hidden transform transition duration-500 hover:scale-105">
       <div className="relative ">
@@ -35,11 +36,17 @@ function Project({ img, title, url, desc }) {
       <div className="p-4">
         <div className='flex justify-between items-center mb-2'>
           <h1 className="text-xl md:text-2xl font-semibold text-gray-800">{title}</h1>
-          <Link href={url} passHref
-            className="text-blue-500 hover:bg-blue-600 hover:text-blue-100 py-2 px-5 rounded-full outline transition-colors duration-300" target='_blank'>
-              Peak🙄
-            
-          </Link>
+          <div className='flex gap-4'>
+            <Link href={url} passHref
+              className="text-blue-500 hover:bg-blue-600 hover:text-blue-100 py-2 px-5 rounded-full outline transition-colors duration-300" target='_blank'>
+                Lihat🙄
+            </Link>
+            <Link href={github} passHref
+              className="text-black text-xl hover:bg-slate-900 hover:text-slate-100 py-2 px-5 rounded-full outline transition-colors duration-300" target='_blank'>
+                <BsGithub/>
+            </Link>
+          </div>
+          
         </div>
         <p className="text-gray-600" dangerouslySetInnerHTML={{ __html: desc || "" }}></p>
       </div>
--- components/Data.js
+++ components/Data.js
@@ -79,30 +79,58 @@ export const catalogData = [
 export const projectData = [
     {
         id:1,
-        title: "Educaplay",
+        title: "ShalatYuk",
         desc: "WebApp untuk Belajar Shalat dengan menyenangkan dan interaktif. <br/> Tech Stack: Next.JS, Postgres, NextAuth, Tailwindcss",
         image: "/project/educa.png",
-        slug: "https://educaplay.vercel.app"
+        slug: "https://educaplay.vercel.app",
+        github: "https://github.com/Frisyk/educaplay"
+    },
+    {
+        id:5,
+        title: "Batika (Belajar Etika)",
+        desc: "Belajar moral dan etika melalui WebApp yang menarik dan menyenangkan. <br/> Tech Stack: Next.JS, MongoDB, NextAuth, Tailwindcss",
+        image: "/project/batika.png",
+        slug: "https://batika.vercel.app",
+        github: "https://github.com/Frisyk/next-mongo"
+    },
+    {
+        id:8,
+        title: "Advanced Notes App",
+        desc: "Aplikasi pengelolaan catatan dengan fitur CRUD, ubah tema dan bahasa serta AUthentikasi <br/> Tech Stack: React.Js, Context",
+        image: "/project/noteV2.png",
+        slug: "https://react-notes-app-self.vercel.app/",
+        github: "https://github.com/Frisyk/react-notesApp"
     },
     {
         id:2,
         title: "GizBiku",
         desc: "Website yang berisi tentang 10 Pesan Gizi Seimbang disertai penjelasan detailnya. <br/> Tech Stack: Next.JS, Tailwindcss",
         image: "/project/giz.png",
-        slug: "https://gizbiku.vercel.app"
+        slug: "https://gizbiku.vercel.app",
+        github: "https://github.com/Frisyk/gizibiku"
     },
     {
         id:3,
         title: "Notes App",
         desc: "Penyimpan catatan sederhana yang terdapat fitur CRUD. <br/> Tech Stack: React.JS",
         image: "/project/note.png",
-        slug: "https://frisyk-notes-apps.vercel.app"
+        slug: "https://frisyk-notes-apps.vercel.app",
+        github: "https://github.com/Frisyk/notes-app"
+    },
+    {
+        id:9,
+        title: "Threads App",
+        desc: "Forum bertanya dan berkomentar dengan fitur CRUD dan authentikasi. <br/> Tech Stack: React.JS, Redux",
+        image: "/project/threads.png",
+        slug: "https://threads-app-one-tan.vercel.app/",
+        github: "https://github.com/Frisyk/thread-app"
     },
     {
         id:4,
         title: "3D Website",
         desc: "Website 3D yang berisi lorem ipsum dolor sit amet. <br/> Tech Stack: Three.JS",
         image: "/project/3d.png",
-        slug: "https://frisyk-threejs-website.vercel.app"
+        slug: "https://frisyk-threejs-website.vercel.app",
+        github: "https://github.com/Frisyk/personal-threejs-website-"
     },
 ]
